33e92df3f8b382c23721d59e589f8188e8805f20
Los' Alexander <sasha.los.0148@gmail.com>
2020-10-12 18:04:47 +0600

    feat: enable bsv sign & successfully tested it

--- src/blockchains/bitcoinsv.ts
+++ src/blockchains/bitcoinsv.ts
@@ -47,4 +47,58 @@ export class BitcoinSV extends BitcoinBase {
         this.networkConfig = this.networks[network].config
         this.defaultPath = this.networks[network].path
     }
+
+    async sign(data: string, keysMap: string): Promise<string> {
+        let dataObj: TransactionForSign, privateKeys: string[]
+        try {
+            dataObj = JSON.parse(data)
+            const parsedKeysMap: Record<string, string> = JSON.parse(keysMap)
+            privateKeys = Object.values(parsedKeysMap)
+        } catch (e) {
+            throw new Error(`data must be a JSON string: ${e.toLocaleString()}`)
+        }
+        const inputs: Array<{
+            txId: string
+            address: string
+            amount: number
+            script: string
+            index: number
+        }> = dataObj.inputs.map(input => {
+            if (
+                input.scriptPubKeyHex === undefined ||
+                input.txId === undefined ||
+                input.value === undefined ||
+                input.n === undefined
+            ) {
+                throw new Error(`Wrong input: ${JSON.stringify(input)}`)
+            }
+            return {
+                txId: input.txId,
+                index: input.n,
+                script: input.scriptPubKeyHex,
+                address: input.address,
+                amount: parseFloat(input.value),
+            }
+        })
+        const outputs: Array<{
+            address: string
+            amount: number
+        }> = dataObj.outputs.map(output => {
+            if (output.amount === undefined || output.address === undefined) {
+                throw new Error(`Wrong output: ${JSON.stringify(output)}`)
+            }
+            return {
+                address: output.address,
+                amount: parseFloat(output.amount),
+            }
+        })
+        const transaction = {
+            inputs,
+            outputs,
+            sum: parseFloat(dataObj.sum),
+            fee: parseFloat(dataObj.fee),
+            keyPairs: privateKeys.map(key => ({ privateKey: key })),
+        }
+        return signBSV(transaction)
+    }
 }
